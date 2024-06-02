1ab6237ac94e8f2679cd58e0e84b616ab495f8bd
AYUB THAPA <68695090+AYUB-CMD@users.noreply.github.com>
2024-06-02 23:05:01 +0530

    frontend setup done

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import FileUpload from './components/file_upload'
+
+function App() {
+  return (
+  
+<>
+<nav class="navbar navbar-light bg-light">
+      <div class="container-fluid">
+        <span class="navbar-brand mb-0 h1">Athelete Classification</span>
+      </div>
+    </nav>
+    <FileUpload/>
+</>
+  );
+}
+
+export default App;
